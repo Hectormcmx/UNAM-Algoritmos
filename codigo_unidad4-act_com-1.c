93fd484f8631e090450d2527957f8b27cc2b7a4c
hectormartinezc <100309699+hectormartinezc@users.noreply.github.com>
2024-04-05 16:06:37 -0600

    Se carga la primera versión del código

---
+++ codigo_unidad4-act_com-1.c
@@ -0,0 +1,29 @@
+/* Universidad Nacional Autónoma de México */
+/* Facultad de contaduría y administración */
+/*Sistema Universidad Abierta y Educación a Distancia*/
+/*Informática modalidad a distancia*/
+/*Asignatura: Diseño de algoritmos*/
+/*Asesor: Juan Manuel Martínez Fernández*/
+/*Alumno: Héctor Martínez Calderón*/
+/*Grupo: 8192*/
+/*Unidad 4: Análisis de problemas*/
+/*Actividad: Actividad Complementaria 1*/
+/*Fecha de entrega: 05 de abril 2024*/
+
+
+/* Programa: */
+
+#include <conio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>
+
+
+int main()
+{
+
+    setlocale(LC_ALL,"");
+    SetConsoleCP(1252);
+    SetConsoleOutputCP(1252);
+    
+}
