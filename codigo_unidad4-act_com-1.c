3cbe9a3adc4401617b7badd9f859a8814993886a
Hector Martinez <165842003+Hectormcmx@users.noreply.github.com>
2024-04-06 00:53:48 +0000

    version final del codigo para la actividad complementaria 1 de la unidad 4

--- codigo_unidad4-act_com-1.c
+++ codigo_unidad4-act_com-1.c
@@ -1,29 +1,72 @@
-/* Universidad Nacional Autónoma de México */
-/* Facultad de contaduría y administración */
-/*Sistema Universidad Abierta y Educación a Distancia*/
-/*Informática modalidad a distancia*/
-/*Asignatura: Diseño de algoritmos*/
-/*Asesor: Juan Manuel Martínez Fernández*/
-/*Alumno: Héctor Martínez Calderón*/
-/*Grupo: 8192*/
-/*Unidad 4: Análisis de problemas*/
-/*Actividad: Actividad Complementaria 1*/
-/*Fecha de entrega: 05 de abril 2024*/
+/********************************************************/
+/*Universidad Nacional Autonoma de Mexico               */
+/*Facultad de contaduria y administracion               */
+/*Sistema Universidad Abierta y Educacion a Distancia   */
+/*Informatica modalidad a distancia                     */
+/*Asignatura: Diseno de algoritmos                      */
+/*Asesor: Juan Manuel Martinez Fernandez                */
+/*Alumno: Hector Martinez Calderen                      */
+/*Grupo: 8192                                           */
+/*Unidad 4: Analisis de problemas                       */
+/*Actividad: Actividad Complementaria 1                 */
+/*Fecha de entrega: 05 de abril 2024                    */
+/********************************************************/
 
+/**********************************************************************/
+/*Descripcion                                                         */ 
+/*Este programa calcula algunos parametros de una piscina             */
+/*Datos de entrada:                                                   */
+/*Medidas de la piscina                                               */
+/*Para piscinas rectangulares se necesita: largo, ancho y alto        */
+/*Para piscinas circulares se necesita diametro y alto                */
+/*Datos de salida                                                     */
+/*Capacidad de la piscina en litros                                   */
+/*Cantidad requerida de cloro en gramos                               */
+/**********************************************************************/
 
-/* Programa: */
-
-#include <conio.h>
 #include <stdio.h>
-#include <locale.h>
-#include <windows.h>
+#include <string.h>
 
+char tipo;
+float largo,ancho,alto,volumen,diametro;
+float cloroli,clorols;
 
 int main()
 {
+    printf("Calculadora para piscinas\t\n");
+    printf("Introduzca la letra R si su piscina es rectangular y precione enter\t\n");
+    printf("Introduzca la letra C si su piscina es redonda y precione enter\t\n");
+    scanf( "%c", &tipo );
+    if (!strcmp(&tipo, "R")) {
+        printf("Su piscina es rectangular\t\n");
+        printf("Introduzca la medida del largo en metros\t\n");
+        scanf("%f", &largo);
+        printf("Introduzca la medida del ancho en metros\t\n");
+        scanf("%f", &ancho);
+        printf("Introduzca la medida del alto en metros\t\n");
+        scanf("%f", &alto);
+        volumen = largo*alto*ancho*1000;
+        cloroli = volumen*0.0015;
+        clorols = cloroli*3;
+        printf("Su piscina tiene una capacidad de %f litros\t\n",volumen);
+        printf("Para agregar cloro por primera vez tiene que tener entre 1 y 3 partes por millon\t\n");
+        printf("Por lo que usted tiene que agregar entre %f y %f gramos de cloro en polvo\t\n",cloroli,clorols);
+    }else
+        if (!strcmp(&tipo, "C")){
+            printf("Su piscina es redonda\t\n");
+            printf("Introduzca el diametro de su piscina\t\n");
+            scanf("%f",&diametro);
+            printf("Introduzca la medida del alto\t\n");
+            scanf("%f",&alto);
+            volumen = 3.141592*(diametro/2)*(diametro/2)*alto*1000;
+            cloroli = volumen*0.0015;
+            clorols = cloroli*3;
+            printf("Su piscina tiene una capacidad de %f litros\t\n", volumen);
+            printf("Para agregar cloro por primera vez tiene que tener entre 1 y 3 partes por millon\t\n");
+            printf("Por lo que usted tiene que agregar entre %f y %f gramos de cloro en polvo\t\n",cloroli,clorols);
+        }else{
+            printf("No se selecciono una opcion correcta\t\n");
+        }
 
-    setlocale(LC_ALL,"");
-    SetConsoleCP(1252);
-    SetConsoleOutputCP(1252);
-    
+    return 0;
 }
